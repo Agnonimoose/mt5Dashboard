bd411a673541b95c82781b09356f260e07bab3ff
Agnonimoose <admin@athena-net.com>
2023-05-03 02:43:17 +0100

    .

--- alitrader/alitrader/static/js/inspectWallet.js
+++ alitrader/alitrader/static/js/inspectWallet.js
@@ -1,10 +1,15 @@
-function updateFollow(){
-    console.log("");
-}
 
 const weeklyLabels = ['Week 1','Week 2', 'Week 3', 'Week 4'];
 const PuBu9 = ["#fff7fb","#ece7f2","#d0d1e6","#a6bddb","#74a9cf","#3690c0","#0570b0","#045a8d","#023858"];
 const PuBu4 = ["#d0d1e6","#74a9cf","#0570b0","#023858"];
+const ctx = document.getElementById('overview');
+
+const swapsCanvas = document.getElementById('swapsPieCard');
+const drawdownCardCanvas = document.getElementById('drawdownCard');
+const weeklyTradeVolsCardCanvas = document.getElementById('weeklyTradeVolsCard');
+const weeklyROIPLCardCanvas = document.getElementById('weeklyROI&PLCard');
+// const graphs = {};
+
 
 
 
@@ -16,7 +21,41 @@ function randoArray(n){
   return tmp;
 }
 
-const ctx = document.getElementById('overview');
+function findWallet(){
+  console.log("findWallet");
+  for (let id in Chart.instances) {
+    Chart.instances[id].destroy();
+  }
+
+  makeSwaps(false);
+  makeDraws(false);
+  makeTradeVols(false);
+  makeROIPL(false);
+}
+
+
+function updateOverview(){
+  overviewChart.destroy();
+  // const labels = Utils.months({count: 7});
+  const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56, 55, 40],
+      fill: false,
+      borderColor: 'rgb(75, 192, 192)',
+      tension: 0.1
+    }]
+  };
+
+  overviewChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+    });
+}
+
+
 var overviewChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -29,6 +68,8 @@ var overviewChart = new Chart(ctx, {
         }]
     },
     options: {
+        responsive: true,
+
         scales: {
         y: {
             beginAtZero: true
@@ -39,150 +80,151 @@ var overviewChart = new Chart(ctx, {
     });
 
 
-function updateOverview(){
-    overviewChart.destroy();
-    // const labels = Utils.months({count: 7});
-    const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
-    const data = {
-      labels: labels,
-      datasets: [{
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-      }]
-    };
 
-    overviewChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
+function makeSwaps(data){
+  if (data === false){
+    data = randoArray(4);
+  }
+
+  var swapsChart = new Chart(swapsCanvas, {
+      type: 'polarArea',
+      data: {
+          labels: weeklyLabels,
+          datasets: [{
+            label: 'My First Dataset',
+            data: data,
+            backgroundColor: PuBu4,
+            hoverOffset: 4,
+            
+          }]
+        },
+      options: {
+          responsive: true,
+          fill: true,
+          plugins: {
+            legend: {
+              display: false,
+            },
+          }
+      }
       });
+    }
+
+function makeDraws(data){
+  if (data === false){
+    data = randoArray(4);
+  }
+
+  var drawdownChart = new Chart(drawdownCardCanvas, {
+    type: 'line',
+    data: {
+          labels: weeklyLabels,
+          datasets: [{
+              data: data,
+              fill: true,
+              borderColor: PuBu4.slice(-1),
+              backgroundColor: PuBu4.slice(-2),
+              tension: 0.1
+            }],
+            hoverOffset: 4,
+        },
+    options: {
+      scales: {
+        y: { display: false },
+        x: {display: false}
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      }
+    }
+  });
 }
 
 
-const swapsCanvas = document.getElementById('swapsPieCard');
-var swapsChart = new Chart(swapsCanvas, {
-    type: 'polarArea',
-    data: {
-        labels: weeklyLabels,
-        datasets: [{
-          label: 'My First Dataset',
-          data: [300, 50, 100, 33],
-          backgroundColor: PuBu4,
-          hoverOffset: 4,
-          
-        }]
-      },
+function makeTradeVols(data){
+  if (data === false){
+    data = randoArray(4);
+  }
+
+  var weeklyTradeVolsChart = new Chart(weeklyTradeVolsCardCanvas, {
+    type: 'bar', 
+    data:{
+      labels: weeklyLabels,
+      datasets: [{
+        data: data,
+        backgroundColor: PuBu4,
+        borderColor: PuBu4,
+        borderWidth: 1
+      }]
+    },
     options: {
-        responsive: true,
-        fill: true,
-        plugins: {
-          legend: {
-            display: false,
-          },
-        }
-    }
-    });
+      scales: {
+        y: { display: false },
+        x: {display: false}
 
-
-const drawdownCardCanvas = document.getElementById('drawdownCard');
-const drawData = {
-        labels: weeklyLabels,
-        datasets: [{
-            data: [0, 8, 6, -2],
-            fill: true,
-            borderColor: PuBu4.slice(-1),
-            backgroundColor: PuBu4.slice(-2),
-            tension: 0.1
-          }],
-          hoverOffset: 4,
-      };   
-var drawdownChart = new Chart(drawdownCardCanvas, {
-  type: 'line',
-  data: drawData,
-  options: {
-    scales: {
-      y: { display: false },
-      x: {display: false}
-    },
-    plugins: {
-      legend: {
-        display: false,
       },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      }
     }
-  }
-});
-
-
-const weeklyTradeVolsCardCanvas = document.getElementById('weeklyTradeVolsCard');
-const weeklyTradeVolsData = {
-  labels: weeklyLabels,
-  datasets: [{
-    data: [65, 59, 80, 81],
-    backgroundColor: PuBu4,
-    borderColor: PuBu4,
-    borderWidth: 1
-  }]
-};
-const weeklyTradeVolsChart = new Chart(weeklyTradeVolsCardCanvas, {
-  type: 'bar', 
-  data:weeklyTradeVolsData,
-  options: {
-    scales: {
-      y: { display: false },
-      x: {display: false}
-
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-    }
-  }
-});
-
-
-
-
-
-const weeklyROIPLCardCanvas = document.getElementById('weeklyROI&PLCard');
-const ROIPLData = {
-        labels: weeklyLabels,
-        datasets: [{
-          label: 'PL',
-            data: randoArray(4),
-            // fill: true,
-            borderColor: PuBu4.slice(-1),
-            backgroundColor: PuBu4.slice(-3),
-            tension: 0.1
-          },
-          {
-            label: 'ROI',
-              data: randoArray(4),
-              fill: false,
-              borderColor: PuBu4.slice(-4),
-              backgroundColor: PuBu4.slice(-4),
-              tension: 0.1
-            },
-        ],
-          hoverOffset: 4,
-      };   
-var ROIPLChart = new Chart(weeklyROIPLCardCanvas, {
-  type: 'line',
-  data: ROIPLData,
-  options: {
-    scales: {
-      y: { display: false },
-      x: {display: false}
-
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-    }
-  }
-  
   });
+}
+
+function makeROIPL(data){
+  if (data === false){
+    var data1 = randoArray(4);
+    var data2 =  randoArray(4);
+  }
+  var ROIPLChart = new Chart(weeklyROIPLCardCanvas, {
+    type: 'line',
+    data: {
+      labels: weeklyLabels,
+      datasets: [{
+        label: 'PL',
+          data: data1,
+          // fill: true,
+          borderColor: PuBu4.slice(-1),
+          backgroundColor: PuBu4.slice(-3),
+          tension: 0.1
+        },
+        {
+          label: 'ROI',
+            data: data2,
+            fill: false,
+            borderColor: PuBu4.slice(-4),
+            backgroundColor: PuBu4.slice(-4),
+            tension: 0.1
+          },
+      ],
+        hoverOffset: 4,
+    },
+    options: {
+      scales: {
+        y: { display: false },
+        x: {display: false}
+
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+      }
+    }
+    
+    });
+}
+
+
+function reizeGraphs(){
+  for (let id in Chart.instances) {
+    Chart.instances[id].resize();
+  }
+}
+
+window.onresize = reizeGraphs;
+
 
